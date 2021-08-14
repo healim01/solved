5a2c37313b7bebd018124e853eb7d0ca36bc336d
healimfx <22000770@handong.edu>
2021-08-14 17:55:07 +0900

    day 5

---
+++ c++/d5.cpp
@@ -0,0 +1,12 @@
+https://www.acmicpc.net/problem/10172
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\"  << endl;
+    cout << "|\"^\"`    |"  << endl;
+    cout << "||_/=\\\\__|"  << endl;
+}
