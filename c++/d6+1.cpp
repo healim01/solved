c4175b7a00d5b4c2f8e70a6dfd032f5da426b9f0
healimfx <22000770@handong.edu>
2021-08-15 14:38:59 +0900

    extra

---
+++ c++/d6+1.cpp
@@ -0,0 +1,7 @@
+// https://www.acmicpc.net/problem/10718
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "강한친구 대한육군" << endl << "강한친구 대한육군" << endl;
+}
---
+++ c++/d6+5.cpp
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/1008
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    cin >> a >> b;
+    cout.precision(15);
+    cout << a/b << endl;
+}
