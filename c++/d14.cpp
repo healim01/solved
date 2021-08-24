0c0b97d8bee3544c92d6cbdd0bf5777cc9067c6d
healimfx <22000770@handong.edu>
2021-08-24 13:09:47 +0900

    day 14

---
+++ c++/d14.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/15552
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n,a,b;
+    cin >> n;
+
+    for (int i=0; i<n; i++) {
+        cin >> a >> b;
+        cout << a+b << "\n";
+    }
+}
