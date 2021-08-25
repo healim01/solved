2f651a7fe46bc188769db566a12b8ad763a8415e
healimfx <22000770@handong.edu>
2021-08-25 13:58:43 +0900

    day 15

---
+++ c++/d15.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/2741
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i=1; i<=n; i++) {
+        cout << i << "\n";
+    }
+}
