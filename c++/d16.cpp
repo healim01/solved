c0e489d41f925cf994dfed69e46db63696cd01e2
healimfx <22000770@handong.edu>
2021-08-26 15:38:07 +0900

    day 16

---
+++ c++/d16.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/2742
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i=n; i>0; i--) {
+        cout << i << "\n";
+    }
+}
