64dbd6a73bb05ccd93829665a3cd98fba4b3343b
healimfx <22000770@handong.edu>
2021-08-30 11:44:38 +0900

    day 19

---
+++ c++/d19.cpp
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/2438
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<=i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
