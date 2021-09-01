072080defc2952174b67d61abd54b25912159e48
healimfx <22000770@handong.edu>
2021-09-01 19:37:55 +0900

    day 21

---
+++ c++/d21.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/10871
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x, a;
+    cin >> n >> x;
+
+    for (int i=0; i<n; i++) {
+        cin >> a;
+        if (a < x) cout << a << " ";
+    }
+}
