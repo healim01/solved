d6c999b7367f689f8167d7bcefb7306911dba2d6
healimfx <22000770@handong.edu>
2021-09-02 15:23:04 +0900

    day 22

---
+++ c++/d22.cpp
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/10951 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    while (cin >> a >> b) {
+        cout << a+b << "\n";
+    }
+}
