a9e86fad19b63889e7e02ffa08f9a1973b991f31
healimfx <22000770@handong.edu>
2021-09-25 13:58:19 +0900

    Create d41.cpp

---
+++ c++/d41.cpp
@@ -0,0 +1,21 @@
+// https://www.acmicpc.net/problem/2445
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<=i; j++) cout << "*";
+        for (int j=0; j<2*(n-i-1); j++) cout << " ";
+        for (int j=0; j<=i; j++) cout << "*";
+        cout << "\n";
+    }
+    for (int i=1; i<n; i++) {
+        for (int j=0; j<n-i; j++) cout << "*";
+        for (int j=0; j<2*i; j++) cout << " ";
+        for (int j=0; j<n-i; j++) cout << "*";
+        cout << "\n";
+    }
+}
