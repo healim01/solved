61d969a4af33b801ab5e7fdb07be7f04925101ad
healimfx <22000770@handong.edu>
2021-09-26 14:11:20 +0900

    Create d42.cpp

---
+++ c++/d42.cpp
@@ -0,0 +1,19 @@
+// https://www.acmicpc.net/problem/2446
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<i; j++) cout << " ";
+        for (int j=0; j<2*(n-i)-1; j++) cout << "*";
+        cout << "\n";
+    }
+    for (int i=1; i<n; i++) {
+        for (int j=0; j<n-i-1; j++) cout << " ";
+        for (int j=0; j<=2*i; j++) cout << "*";
+        cout << "\n";
+    }
+}
