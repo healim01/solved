d7e575f24116f0442c9b1c630d5e62f4acf9f989
healimfx <22000770@handong.edu>
2021-09-27 23:59:47 +0900

    Update d43.cpp

--- c++/d43.cpp
+++ c++/d43.cpp
@@ -1,10 +1,15 @@
-//
+// https://www.acmicpc.net/problem/10872
 #include <iostream>
 using namespace std;
+
+int fact(int n) {
+    if (n<2) return 1;
+    else return n * fact(n-1);
+}
 
 int main() {
     int n;
     cin >> n;
 
-    
+    cout << fact(n) << "\n";
 }
