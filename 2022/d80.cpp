92a503eecd659c2092f76a15dc09a316b6b4ce49
healim01 <22000770@handong.edu>
2022-03-24 17:05:36 -0500

    Create d80.cpp

---
+++ 2022/d80.cpp
@@ -0,0 +1,28 @@
+// https://www.acmicpc.net/problem/10773
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, a, sum = 0;
+    vector<int> num;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        if (a != 0)
+            num.push_back(a);
+        else
+        {
+            num.pop_back();
+        }
+    }
+
+    for (int i = 0; i < num.size(); i++)
+    {
+        sum += num[i];
+    }
+    cout << sum << "\n";
+}
