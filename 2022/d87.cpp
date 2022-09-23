feac830fee9774495af4cb3d6d0d7c4cfab2d13a
healim01 <22000770@handong.edu>
2022-09-23 15:56:21 -0500

    Create d87.cpp

---
+++ 2022/d87.cpp
@@ -0,0 +1,25 @@
+// https://www.acmicpc.net/problem/10992
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+            cout << " ";
+        cout << "*";
+        if (i != 0)
+        {
+            for (int j = 0; j < 2 * i - 1; j++)
+                cout << " ";
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for (int i = 0; i < 2 * n - 1; i++)
+        cout << "*";
+}
