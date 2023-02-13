86b935b4e0ecc5f682c56fcbc0f6095d742a992a
healim01 <22000770@handong.edu>
2023-02-13 22:37:38 +0900

    Create d101.cpp

---
+++ 2023/d101.cpp
@@ -0,0 +1,25 @@
+// https://www.acmicpc.net/problem/2164
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int n;
+    queue<int> card;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        card.push(i);
+    }
+
+    while (card.size() > 1)
+    {
+        card.pop();
+        card.push(card.front());
+        card.pop();
+    }
+
+    cout << card.front() << "\n";
+}
