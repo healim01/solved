ccc48f6db972cf98ba2b8c413a78dc160bd1f0ba
Hailey <74346290+healim01@users.noreply.github.com>
2024-11-17 17:14:08 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 9.04 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -0,0 +1,48 @@
+function solution(friends, gifts) {
+    
+    const nameIndexMap = {};
+    friends.forEach((name, index) => {
+      nameIndexMap[name] = index;
+    });
+    
+    const N = friends.length;
+    const giftMatrix = Array.from({ length: N }, () => Array(N).fill(0));
+    
+   const giftNum = friends.reduce((obj, name) => {
+      obj[name] = 0;
+      return obj;
+    }, {});
+    
+    gifts.map((gift) => {
+        const gi = gift.split(" ");
+        
+        giftNum[gi[0]] += 1; 
+        giftNum[gi[1]] -= 1; 
+        
+        const senderIndex = nameIndexMap[gi[0]];
+        const receiverIndex = nameIndexMap[gi[1]];
+
+        giftMatrix[senderIndex][receiverIndex] += 1;
+    })
+    
+    let maxGift = 0;
+    for (let i = 0; i < N; i += 1) {
+        let nextGift = 0;
+        for (let j = 0; j < N; j += 1) {
+            if (giftMatrix[i][j] > giftMatrix[j][i]) {
+                nextGift += 1;
+            } else if (giftMatrix[i][j] == giftMatrix[j][i]) {
+                if (giftNum[friends[i]] > giftNum[friends[j]]) {
+                    nextGift += 1;
+                }
+            }
+        }
+        if (nextGift > maxGift) {
+            maxGift = nextGift;
+        }
+    }
+    
+    
+    
+    return maxGift;
+}
