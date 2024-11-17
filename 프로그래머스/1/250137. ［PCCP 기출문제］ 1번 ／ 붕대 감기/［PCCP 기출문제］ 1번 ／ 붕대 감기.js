dcf7ebc78ec7fa541eb704b5f2a258c9734d6f2a
Hailey <74346290+healim01@users.noreply.github.com>
2024-11-17 17:14:51 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.41 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -0,0 +1,40 @@
+function solution(bandage, health, attacks) {
+    
+    let time = 0;
+    let tryTime = 0; 
+    const maxHealth = health; 
+    
+    while (health > 0) {
+        time += 1; 
+        tryTime += 1; 
+        
+        if (!attacks.length) break;
+        
+        let attacked = false;
+        if (attacks[0][0] === time) {
+            health -= attacks[0][1];
+            
+            tryTime = 0;
+            attacks.shift();
+            
+            attacked = true;
+        }
+        
+        if (health <= 0) break; 
+        
+        if (!attacked) {
+            health += bandage[1];
+        
+            if (tryTime === bandage[0]) {
+                health += bandage[2]; 
+                tryTime = 0;
+            }
+            
+            if (health > maxHealth) health = maxHealth;
+            
+        }
+        
+    }
+    
+    return health > 0 ? health : -1;
+}
