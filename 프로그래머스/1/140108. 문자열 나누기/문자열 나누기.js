50fe13896d22ca59d7657dcd48b7bc28498ebde9
Hailey <74346290+healim01@users.noreply.github.com>
2024-11-26 14:34:24 +0900

    Update 문자열 나누기.js

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -5,13 +5,11 @@ function solution(s) {
     
     let xCnt = 0, yCnt = 0;
     Array.from(s).forEach((a) => {
-        // console.log(a);
         if (x === null) x = a;
         
         if (a === x) xCnt++; 
         else yCnt++; 
         
-        // console.log(x, xCnt, yCnt);
         if (xCnt === yCnt) {
             word++; 
             x = null; 
