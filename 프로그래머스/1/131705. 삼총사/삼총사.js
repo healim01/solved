f48e6720462229371151ff452dbd542c8910650c
Hailey <74346290+healim01@users.noreply.github.com>
2024-12-04 13:38:09 +0900

    [level 1] Title: 삼총사, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -2,15 +2,11 @@ function solution(number) {
     var answer = 0;
     
     for (let i=0; i<number.length; i++) {
-        let three = number[i];
         for (let j=i+1; j<number.length; j++) {
-            three += number[j]; 
             for (let k=j+1; k<number.length; k++) {
-                three += number[k]; 
+               let three = number[i] + number[j] + number[k];
                 if (three === 0) answer++; 
-                three -= number[k];
             }
-            three -= number[j];
         }
     }
     
