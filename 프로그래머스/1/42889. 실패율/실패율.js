9605eae18cfbca99d45fa8d4f8af9e3b5ad0a4ba
Hailey <74346290+healim01@users.noreply.github.com>
2025-02-26 18:46:39 +0900

    [level 1] Title: 실패율, Time: 1723.10 ms, Memory: 48.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -0,0 +1,20 @@
+function solution(N, stages) {
+    var answer = [];
+    let fail = 0; 
+    
+    stages.sort((a,b) => a-b);
+    
+    for (let i=1; i<=N; i++) {
+        let count = stages.filter(s => s === i).length; 
+       
+        answer.push([i, count / (stages.length - fail)])
+        fail += count;
+    }
+    
+    answer.sort((a, b) => b[1] - a[1] || a[0] - b[0]);
+    
+    return answer.map((a) => a[0]);
+}
+
+// 1 2 2 3 3 3 4 6 
+
