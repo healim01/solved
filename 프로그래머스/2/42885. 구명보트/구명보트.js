f8f348edc15056c77b38f0f8570f95cd435a4676
Hailey <74346290+healim01@users.noreply.github.com>
2025-03-03 16:08:40 +0900

    [level 2] Title: 구명보트, Time: 13.43 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,18 @@
+function solution(people, limit) {
+    let count = 0;
+    people.sort((a,b) => b - a); 
+    
+    let skinny = people.length - 1; 
+    
+    for (let i=0; i<people.length; i++) {
+        if (i > skinny) break; 
+     
+        if (people[i] + people[skinny] <= limit) {
+            skinny--;
+        }
+        
+        count++; 
+    }  
+    
+    return count;
+}
