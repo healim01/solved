cbdab6a7766ceeb7f630c8ef16c68d301ed0bf59
Hailey <74346290+healim01@users.noreply.github.com>
2025-03-21 15:08:14 +0900

    [level 2] Title: 가장 큰 수, Time: 189.21 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,15 +1,4 @@
 function solution(numbers) {
-    // return numbers.sort((a,b) => Number(`${b}${a}`) > Number(`${a}${b}`) ? 1 : -1).join('');
-    
-     const sorted = numbers.sort((a, b) => {
-        const ab = `${a}${b}`;
-        const ba = `${b}${a}`;
-        return ba - ab;
-    });
-
-    // 정렬된 배열을 이어 붙여서 결과 문자열을 만듬
-    const result = sorted.join('');
-
-    // 결과가 "0"만 있으면, "0"을 반환
+    const result = numbers.sort((a,b) => `${b}${a}` - `${a}${b}`).join('');
     return result[0] === '0' ? '0' : result;
 }
