9866781aacee27293e9d0fd7858df05fa1b0e0fb
Hailey <74346290+healim01@users.noreply.github.com>
2025-03-23 20:15:05 +0900

    [level 1] Title: 약수의 합, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,14 @@
+function solution(n) {
+     let answer = 0;
+    
+     for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            answer += i; 
+            if (i !== n / i) {
+                answer += n / i;
+            }
+        }
+    }
+    
+    return answer;
+}
