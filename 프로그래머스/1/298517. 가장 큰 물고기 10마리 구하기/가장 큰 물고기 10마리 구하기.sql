117f07f4aaea77074eb736b77e9f9b406db5fb56
Hailey <74346290+healim01@users.noreply.github.com>
2025-05-06 00:38:46 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -1,3 +1,7 @@
 -- 코드를 작성해주세요
 
-select id, length from FISH_INFO where length is not null order by length desc, id limit 10;
+select id, length 
+from FISH_INFO 
+where length is not null 
+order by length desc, id 
+limit 10;
