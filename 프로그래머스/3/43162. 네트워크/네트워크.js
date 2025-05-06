237bc286459152a738a59be8ad48b7bc974ae766
Hailey <74346290+healim01@users.noreply.github.com>
2025-05-06 23:19:29 +0900

    [level 3] Title: 네트워크, Time: 0.48 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,29 +1,30 @@
 function solution(n, computers) {
-  let answer = 0;
-  const visited = Array(n).fill(false);
-
-  const bfs = (start) => {
-    const queue = [start];
-    visited[start] = true;
-
-    while (queue.length) {
-      const node = queue.shift();
-
-      for (let i = 0; i < n; i++) {
-        if (computers[node][i] === 1 && !visited[i]) {
-          visited[i] = true;
-          queue.push(i);
+    let answer = 0;
+    const visited = Array(n).fill(false);
+    
+    const bfs = (start) => {
+        const queue = [start]; 
+        visited[start] = true; 
+        
+        while (queue.length) {
+            const node = queue.shift();
+            
+            for (let j=0; j<n; j++) {
+                if (computers[node][j] === 1 && !visited[j]) {
+                    visited[j] = true; 
+                    queue.push(j); 
+                }
+            }  
+        } 
+    }
+    
+    
+    for (let i=0; i<n; i++) {
+        if (!visited[i]) {
+            bfs(i); 
+            answer++; 
         }
-      }
     }
-  };
-
-  for (let i = 0; i < n; i++) {
-    if (!visited[i]) {
-      bfs(i);
-      answer++;
-    }
-  }
-
-  return answer;
+    
+    return answer;
 }
