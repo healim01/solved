1ecd72082378d6354ec8656b34b0c832e35a5164
Hailey <74346290+healim01@users.noreply.github.com>
2025-05-08 01:13:33 +0900

    [level 2] Title: 큰 수 만들기, Time: 39.25 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,19 +1,40 @@
-function solution(number, k) {
-    let answer = []; 
-    let cnt = 0; 
+// function solution(number, k) {
+//     let answer = []; 
+//     let cnt = 0; 
     
-    [...number].forEach((n) => {            
-        while (answer.length > 0 && answer[answer.length - 1] < n && cnt < k) {
-            answer.pop(); 
-            cnt++;
+//     [...number].forEach((n) => {            
+//         while (answer.length > 0 && answer[answer.length - 1] < n && cnt < k) {
+//             answer.pop(); 
+//             cnt++;
+//         }
+        
+//         answer.push(n);
+//     })
+    
+//      if (answer.length > number.length - k) {
+//         answer = answer.slice(0, number.length - k);
+//     }
+    
+//     return answer.join('');
+// }
+
+function solution(number, k) {
+    const stack = []; 
+    
+    let trash = 0; 
+    [...number].forEach((n) => {
+        while (trash < k && stack.length !== 0) {
+            const prev = stack.pop(); 
+            
+            if (prev < n) trash++; 
+            else {
+                stack.push(prev); 
+                break;
+            }
         }
         
-        answer.push(n);
+        stack.push(n);
     })
     
-     if (answer.length > number.length - k) {
-        answer = answer.slice(0, number.length - k);
-    }
-    
-    return answer.join('');
+    return stack.slice(0, number.length - k).join('');
 }
