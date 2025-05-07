efc04685b208b401f5f6c4a237841fb1553f3d3d
Hailey <74346290+healim01@users.noreply.github.com>
2025-05-08 03:03:03 +0900

    [level 1] Title: 최소직사각형, Time: 26.38 ms, Memory: 40.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,12 +1,24 @@
-function solution(sizes) {
-    var width = 0, length = 0;
+// function solution(sizes) {
+//     var width = 0, length = 0;
     
-    sizes.forEach((size) => {
-       size.sort((a,b) => a - b);
+//     sizes.forEach((size) => {
+//        size.sort((a,b) => a - b);
         
-        if (size[0] > length) length = size[0];
-        if (size[1] > width) width = size[1];
+//         if (size[0] > length) length = size[0];
+//         if (size[1] > width) width = size[1];
+//     })
+    
+//     return width * length;
+// }
+
+function solution(sizes) {
+    sizes = sizes.map((size) => [Math.min(...size), Math.max(...size)]);
+    
+    let width = sizes[0][0], height = sizes[0][1]; 
+    sizes.forEach(([w,h]) => {
+        width = Math.max(w, width); 
+        height = Math.max(h, height); 
     })
     
-    return width * length;
+    return width * height;
 }
