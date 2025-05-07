67e1d99beb57f5ca74204465d31eebdd0b796134
Hailey <74346290+healim01@users.noreply.github.com>
2025-05-08 00:57:05 +0900

    [level 3] Title: 단속카메라, Time: 21.30 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=javascript) 
 
 ### 성능 요약
 
-메모리: 35.8 MB, 시간: 18.64 ms
+메모리: 36.4 MB, 시간: 21.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 07일 12:04:31
+2025년 05월 08일 00:57:02
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,14 +1,34 @@
+// function solution(routes) {
+//     var answer = 1;
+//     routes.sort((a,b) => a[1] - b[1]); 
+    
+//     let point = routes[0][1]; 
+//     routes.forEach((route) => {
+//       if (route[0] > point ||  point > route[1]) {
+//           point = route[1]; 
+//           answer++; 
+//       }  
+//     })
+    
+//     return answer;
+// }
+
+
 function solution(routes) {
-    var answer = 1;
-    routes.sort((a,b) => a[1] - b[1]); 
+    routes.sort((a,b) => a[1] - b[1]);
     
-    let point = routes[0][1]; 
-    routes.forEach((route) => {
-      if (route[0] > point ||  point > route[1]) {
-          point = route[1]; 
-          answer++; 
-      }  
-    })
+    let flag = 1; 
+    let camera = routes[0][1]; 
     
-    return answer;
+    for (let route of routes) {
+        const [start, end] = route; 
+        
+        if (start > camera || camera > end) {
+            camera = end; 
+            flag++; 
+        }
+    }
+    
+    return flag;
 }
+
