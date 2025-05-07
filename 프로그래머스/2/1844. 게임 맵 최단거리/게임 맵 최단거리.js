6786d0e59809b471146d262294895225189e8411
Hailey <74346290+healim01@users.noreply.github.com>
2025-05-07 15:12:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 26.70 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,60 +1,37 @@
-class Queue {
-    items = [];
-    front = 0; 
-    rear = 0;
-    
-    push(item) {
-        this.items.push(item);
-        this.rear++;
-    }
-    
-    pop() {
-        return this.items[this.front++];
-    }
-    
-    isEmpty() {
-        return this.front === this.rear;
-    }
-}
-
-
 function solution(maps) {
-    var answer = 0;
-    
-    const moves = [[-1, 0], [0, -1], [0, 1], [1, 0]];
-    
-    const n = maps.length;
-    const m = maps[0].length;
-    
-    const visited = Array.from({ length: n }, () => Array(m).fill(-1));
-    
-    function bfs(start) {
-        const q = new Queue(); 
-        q.push(start);
-        visited[start[0]][start[1]] = 1;
+  const direction = [
+    [0, 1], 
+    [0, -1], 
+    [1, 0], 
+    [-1, 0]
+  ];
+  
+  const visited = Array.from({ length: maps.length }, () =>
+    Array(maps[0].length).fill(false)
+  );
+  const queue = [{ position: [0, 0], paths: 1 }];
+  visited[0][0] = true;
 
-        while (!q.isEmpty()) {
-            const here = q.pop(); 
-            
-            for (let [dx, dy] of moves) {
-                const row = here[0] + dx;
-                const column = here[1] + dy;
-                
-                if (row < 0 || row >= n || column < 0 || column >= m) continue; 
-                if (maps[row][column] === 0) continue;
-                
-                if (visited[row][column] === -1) {
-                    q.push([row, column]);
-                    visited[row][column] = visited[here[0]][here[1]] + 1;
-                }
-            }
-        }
-        
-        return visited;
+  while (queue.length) {
+    const { position, paths } = queue.shift();
+    const [x, y] = position;
+
+    if (x === maps[0].length - 1 && y === maps.length - 1) return paths;
+
+    for (let [dx, dy] of direction) {
+      const newX = x + dx;
+      const newY = y + dy;
+
+      if (
+        newX >= 0 && newY >= 0 &&
+        newX < maps[0].length && newY < maps.length &&
+        maps[newY][newX] === 1 && !visited[newY][newX]
+      ) {
+        visited[newY][newX] = true;
+        queue.push({ position: [newX, newY], paths: paths + 1 });
+      }
     }
-    
-    bfs([0,0])
-    
-    
-    return visited[n - 1][m - 1];
+  }
+
+  return -1;
 }
