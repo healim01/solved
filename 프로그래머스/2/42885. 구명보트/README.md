73c473dee80036e64b9e9c6a3f2d037024d179a4
Hailey <74346290+healim01@users.noreply.github.com>
2025-05-08 01:47:37 +0900

    [level 2] Title: 구명보트, Time: 18.76 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=javascript) 
 
 ### 성능 요약
 
-메모리: 38.4 MB, 시간: 13.43 ms
+메모리: 35.4 MB, 시간: 18.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 03일 16:08:38
+2025년 05월 08일 01:47:35
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,18 +1,36 @@
-function solution(people, limit) {
-    let count = 0;
-    people.sort((a,b) => b - a); 
+// function solution(people, limit) {
+//     let count = 0;
+//     people.sort((a,b) => b - a); 
     
-    let skinny = people.length - 1; 
+//     let skinny = people.length - 1; 
     
-    for (let i=0; i<people.length; i++) {
-        if (i > skinny) break; 
+//     for (let i=0; i<people.length; i++) {
+//         if (i > skinny) break; 
      
-        if (people[i] + people[skinny] <= limit) {
-            skinny--;
-        }
+//         if (people[i] + people[skinny] <= limit) {
+//             skinny--;
+//         }
         
-        count++; 
-    }  
+//         count++; 
+//     }  
     
-    return count;
+//     return count;
+// }
+
+
+function solution(people, limit) {
+    let cnt = 0; 
+    people.sort((a,b) => b-a);
+    
+    let point = people.length -1; 
+    for (let i=0; i<people.length; i++) {
+        if (i > point) break;
+        
+        if (people[i] + people[point] <= limit) {
+            point--; 
+        }
+        cnt++; 
+    }
+
+    return cnt;
 }
