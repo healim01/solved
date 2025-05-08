3b2572e0fd86987177167aebfad70392d669dbfc
Hailey <74346290+healim01@users.noreply.github.com>
2025-05-08 20:36:37 +0900

    [level 3] Title: 정수 삼각형, Time: 9.67 ms, Memory: 43 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=javascript) 
 
 ### 성능 요약
 
-메모리: 43.1 MB, 시간: 7.42 ms
+메모리: 43 MB, 시간: 9.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 12:45:12
+2025년 05월 08일 20:36:34
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,17 +1,34 @@
+// function solution(triangle) {
+//     var answer = 0;
+//     const n = triangle.length;
+//     const dp = Array.from(Array(n), () => Array(n).fill(0));
+    
+//     dp[0][0] = triangle[0][0]; 
+    
+//     for (let i=1; i<n; i++) {
+//       for (let j=0; j<=i; j++) {
+//          if (j === 0) dp[i][j] = triangle[i][j] + dp[i-1][j]; 
+//          else if (j === n-1) dp[i][j] = triangle[i][j] + dp[i-1][j-1];
+//          else dp[i][j] = triangle[i][j] + Math.max(dp[i-1][j-1], dp[i-1][j]); 
+//       }
+//     }
+    
+//     return Math.max(...dp[n-1]);
+// }
+
+
 function solution(triangle) {
-    var answer = 0;
-    const n = triangle.length;
-    const dp = Array.from(Array(n), () => Array(n).fill(0));
+    const arr = Array.from({length: triangle.length}, () => Array(triangle.length).fill(0));
     
-    dp[0][0] = triangle[0][0]; 
+    arr[0][0] = triangle[0][0]; 
+    triangle.forEach((tri, i) => {
+        if (i !== 0) {
+            tri.forEach((t, j) => {
+                if (j === 0) arr[i][j] = t + arr[i-1][j]; 
+                else arr[i][j] = t + Math.max(arr[i-1][j-1], arr[i-1][j]);
+            })
+        }
+    })
     
-    for (let i=1; i<n; i++) {
-      for (let j=0; j<=i; j++) {
-         if (j === 0) dp[i][j] = triangle[i][j] + dp[i-1][j]; 
-         else if (j === n-1) dp[i][j] = triangle[i][j] + dp[i-1][j-1];
-         else dp[i][j] = triangle[i][j] + Math.max(dp[i-1][j-1], dp[i-1][j]); 
-      }
-    }
-    
-    return Math.max(...dp[n-1]);
+    return Math.max(...arr[triangle.length - 1]);
 }
